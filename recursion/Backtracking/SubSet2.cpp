28596135ac7589fc24b0cda9db1b488b3e1d566f
tanu8427 <tk842785@gmail.com>
2025-02-18 14:29:11 +0530

    intial commit

---
+++ recursion/Backtracking/SubSet2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void getDpSubset(vector<int> &arr, vector<int> &ans, int i ){
+
+    if(i==arr.size()){
+        cout<<
+    }
+
+   
+}
+
+int main(){
+ vector<int> arr ={1,2,2};
+ vector<int> ans;
+
+ getDpsubset(arr,ans,0);
+ 
+
+}
